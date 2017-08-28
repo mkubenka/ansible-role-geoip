0b4c0887bdcab834c52712d18fe453c632117f6d
Michal Kubenka <mkubenka@gmail.com>
2017-08-28 14:45:16 +0200

    Change test IP.

--- test/integration/default/default.rb
+++ test/integration/default/default.rb
@@ -2,7 +2,7 @@ describe file('/etc/cron.weekly/geoipupdate') do
   it { should be_mode 0755 }
 end
 
-describe command('geoiplookup -i -f /usr/share/GeoIP/GeoLiteCity.dat 8.8.8.8') do
-  its(:stdout) { should match /US, CA/ }
+describe command('geoiplookup -i -f /usr/share/GeoIP/GeoLiteCity.dat 3.0.0.1') do
+  its(:stdout) { should match /US, CT/ }
   its(:exit_status) { should eq 0 }
 end
