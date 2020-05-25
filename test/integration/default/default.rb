2afdfa6be520b295de6fc4540074ef4d7120e9bb
Michal Kubenka <mkubenka@gmail.com>
2020-05-25 15:21:36 +0200

    Remove geoipupdate. GeoLite version 1 is no longer supported.

--- test/integration/default/default.rb
+++ test/integration/default/default.rb
@@ -1,8 +1,4 @@
-describe file('/etc/cron.weekly/geoipupdate') do
-  it { should be_mode 0755 }
-end
-
-describe command('geoiplookup -i -f /usr/share/GeoIP/GeoLiteCity.dat 3.0.0.1') do
-  its(:stdout) { should match /US, CT/ }
+describe command('geoiplookup -i -f /usr/share/GeoIP/GeoIPCity.dat 3.0.0.1') do
+  its(:stdout) { should match /US, WA/ }
   its(:exit_status) { should eq 0 }
 end
